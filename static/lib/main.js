5c3d8c206b325a4e85608d2647bc0810fcc8833c
Magnus Vhendin <magnus.vhendin@gmail.com>
2022-06-09 09:50:55 +0200

    tooltips for medals

--- static/lib/main.js
+++ static/lib/main.js
@@ -1,7 +1,23 @@
 'use strict';
 
-/* globals */
+/* globals $ */
 
 $(document).ready(function () {
+    $(window)
+        .off('action:ajaxify.end')
+        .on('action:ajaxify.end', () => {
+            const $medals = $('.nodebb-plugin-medals.medal-container .medal');
 
+            for (let i = 0; i < $medals.length; i++) {
+                const $medal = $($medals[i]);
+
+                if ($medal.attr('title')) {
+                    $medal.tooltip({
+                        placement: $medal.attr('title-placement') || 'bottom',
+                        title: $medal.attr('title'),
+                        container: '#content',
+                    });
+                }
+            }
+        });
 });
